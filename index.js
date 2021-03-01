c8fa969291c3c23a076f669471ca6736c9355c30
wangkaiwd <1162486176@qq.com>
2021-03-01 17:27:30 +0800

    feat: handle commit messages

--- index.js
+++
@@ -1,7 +0,0 @@
-const { exec } = require('child_process');
-
-exec('cat package.json', (err, stdout, stderr) => {
-  console.log('err', err);
-  console.log('stdout', stdout);
-  console.log('stderr', stderr);
-});
